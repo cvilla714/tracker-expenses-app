4d870300be8235f872ee92fe2783a5f5196ab750
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-12 22:18:28 -0400

    Added reducer configuration to use the checkLoginStatus as the one to make the calls to the back end in rails, also added the middleware to be use in concat with the reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,13 @@
+/* eslint-disable max-len */
 import { configureStore } from '@reduxjs/toolkit';
+import { checkLoginStatusapi } from '../features/user/statusSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    [checkLoginStatusapi.reducerPath]: checkLoginStatusapi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(checkLoginStatusapi.middleware),
 });
 
 export default store;
