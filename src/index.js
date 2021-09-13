8659347f3751bf737398139db548440eec6b2cc7
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-12 22:02:17 -0400

    Added bootstrap main css file to the project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import store from './app/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
