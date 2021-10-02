7dc76e7986b1c9acd50fc2844e077f933b1f8a67
Cosmel Villalobos <cosmelvg@gmail.com>
2021-10-02 16:14:00 -0400

    Added alert component

---
+++ src/containers/auth/Alert.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Alert = () => (
+  <div className="alert alert-warning" role="alert">
+    Wrong Credentiails, Please try again!
+  </div>
+);
+
+export default Alert;
