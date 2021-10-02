2bddd65bb01cba8adc1979ac65e0ec2499a52bcd
Cosmel Villalobos <cosmelvg@gmail.com>
2021-10-02 16:49:16 -0400

    Added new message for user to let them know they have to log in to get access to the app

--- src/containers/auth/Alert.js
+++ src/containers/auth/Alert.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Alert = () => (
-  <div className="alert alert-warning" role="alert">
-    Wrong Credentiails, Please try again!
-  </div>
-);
+const Alert = () => {
+  useEffect(() => {
+    const timer = setTimeout(() => {}, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="alert alert-warning" role="alert">
+      You need to log in to get access to the app
+    </div>
+  );
+};
 
 export default Alert;
