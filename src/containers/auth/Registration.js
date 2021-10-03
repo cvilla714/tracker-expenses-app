7b15b6d7a2c1a1b9e91d7d77e4434c183ab07a42
Cosmel Villalobos <cosmelvg@gmail.com>
2021-10-03 19:15:07 -0400

    Added registration alert to find help with registration

--- src/containers/auth/Registration.js
+++ src/containers/auth/Registration.js
@@ -1,8 +1,15 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useHistory } from 'react-router';
-import { useRegisterUserMutation } from '../../features/user/statusSlice';
+import {
+  useRegisterUserMutation,
+  // useGetLoginUserInfoQuery,
+} from '../../features/user/statusSlice';
 import useForm from '../../components/Hooks/useForm';
+// import Home from '../../components/Expenses/Home';
+// import SuccessRegistration from './AlertRegistrationInfo';
+import RegistgrationAlert from './AlertRegistration';
 import './Registration.css';
 
 const Registration = () => {
@@ -15,6 +22,7 @@ const Registration = () => {
 
   const history = useHistory();
   const [registerUser] = useRegisterUserMutation();
+  // const { data, error, isLoading } = useGetLoginUserInfoQuery();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -33,56 +41,123 @@ const Registration = () => {
 
   return (
     <div className="container mt-custom">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="name"
-            placeholder="Name"
-            value={form.name}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={form.email}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
+      <>
+        <RegistgrationAlert />
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={form.name}
+              onChange={handleChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={form.email}
+              onChange={handleChange}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={form.password}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId="formBasicPasswordConfirmation">
-          <Form.Label>Password Confirmation</Form.Label>
-          <Form.Control
-            type="password"
-            name="password_confirmation"
-            placeholder="Confirm Password"
-            value={form.password_confirmation}
-            onChange={handleChange}
-            required
-          />
-        </Form.Group>
-        <Button className="mt-5" type="submit">
-          Register
-        </Button>
-      </Form>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={form.password}
+              onChange={handleChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPasswordConfirmation">
+            <Form.Label>Password Confirmation</Form.Label>
+            <Form.Control
+              type="password"
+              name="password_confirmation"
+              placeholder="Confirm Password"
+              value={form.password_confirmation}
+              onChange={handleChange}
+              required
+            />
+          </Form.Group>
+          <Button className="mt-5" type="submit">
+            Register
+          </Button>
+        </Form>
+      </>
+      {/* {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data.logged_in ? (
+        <>
+          <SuccessRegistration />
+          <Home />
+        </>
+      ) : (
+        <>
+          <RegistgrationAlert />
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={form.name}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={form.email}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={form.password}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicPasswordConfirmation">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control
+                type="password"
+                name="password_confirmation"
+                placeholder="Confirm Password"
+                value={form.password_confirmation}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+            <Button className="mt-5" type="submit">
+              Register
+            </Button>
+          </Form>
+        </>
+      )} */}
     </div>
   );
 };
