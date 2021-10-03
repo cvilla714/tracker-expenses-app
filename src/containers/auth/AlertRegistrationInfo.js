4abe2af24b908e2c270489820f6981d2e40e1e95
Cosmel Villalobos <cosmelvg@gmail.com>
2021-10-03 19:12:57 -0400

    Added successfull alert registration

---
+++ src/containers/auth/AlertRegistrationInfo.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { Alert, Button } from 'react-bootstrap';
+
+const AlertRegistrationInfo = () => {
+  const [show, setShow] = useState(true);
+
+  if (show) {
+    return (
+      <Alert variant="warning" onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>Welcome!</Alert.Heading>
+        <p>You have registered successfully</p>
+      </Alert>
+    );
+  }
+  return <Button className="exit" onClick={() => setShow(true)} />;
+};
+export default AlertRegistrationInfo;
